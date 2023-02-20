1386d5b52215a3b9d239b9930665d4285f865e8c
annikalenneper <annika.lenneper@smail.th-koeln.de>
2023-02-20 16:57:57 +0100

    SubmitButton für Ziel implementiert mit Feedback, Speicherung steht aus

--- app/src/main/java/com/example/ep_prototyp/Analysis1.kt
+++ app/src/main/java/com/example/ep_prototyp/Analysis1.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 
 
@@ -23,10 +24,24 @@ class Analysis1 : Fragment() {
 
         button.setOnClickListener {
             findNavController().navigate(R.id.action_analysis1_to_analysis2)
-
         }
 
+        val setGoal = view.findViewById<TextView>(R.id.setGoalText)
+        val submitButton = view.findViewById<Button>(R.id.submitGoal)
+        val feedbackGoal = view.findViewById<TextView>(R.id.feedbackTextGoal)
 
+        submitButton.setOnClickListener(){
+            val goal = setGoal.text
+            //hier Ziel speichern (room)
+            if (setGoal.text == "gib dein Ziel ein" || setGoal.text == ""){
+                feedbackGoal.text = "Bitte gib ein Ziel ein."
+            }
+            else {
+                feedbackGoal.text = "Dein Ziel lautet $goal." +
+                        "Sind alle Kriterien für ein gutes Ziel erfüllt? Dann klicke auf \"Weiter\"" +
+                        "Du kannst noch etwas verbessern? Dann gib dein Ziel erneut ein!"
+            }
+        }
 
         return view
     }
