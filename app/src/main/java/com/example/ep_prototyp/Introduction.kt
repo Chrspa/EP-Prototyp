f0d6000453f29cfc9780506e198eafe5045f4378
annikalenneper <annika.lenneper@smail.th-koeln.de>
2023-02-22 15:40:47 +0100

    Anpassung Margins

--- app/src/main/java/com/example/ep_prototyp/Introduction.kt
+++ app/src/main/java/com/example/ep_prototyp/Introduction.kt
@@ -27,8 +27,9 @@ class Introduction : Fragment() {
         val list = listOf<String>(
             "Mit einem smarten Verhaltens-Design und kleinen Schritten lassen sich Veränderungen nachhaltig gestalten – ganz ohne Selbstvorwürfe und Schamgefühle.",
             "Das funktioniert so:\n\n" +
-                    "o\tDu fängst mit kleinen Verhaltensänderungen an, findest heraus, wie du diese in deinen Alltag integrieren und bringst sie durch stetige positive Bestärkung zur vollen Entfaltung.\n\n" +
-                    "o\tDie Verhalten sind so klein und einfach, dass du dich nicht mit viel Aufwand dazu motivieren musst. Es geht darum, nach und nach neue Gewohnheiten entstehen zu lassen.\n",
+                    "Du fängst mit kleinen Verhaltensänderungen an, findest heraus, wie du diese in deinen Alltag integrieren und bringst sie durch stetige positive Bestärkung zur vollen Entfaltung.",
+            "Das funktioniert so:\n\n" +
+            "Die Verhalten sind so klein und einfach, dass du dich nicht mit viel Aufwand dazu motivieren musst. Es geht darum, nach und nach neue Gewohnheiten entstehen zu lassen.\n",
             "Der feste Grundsatz lautet: „Es gibt kein Versagen!“ Du musst nicht alles gleich sofort können. \n\n Veränderungen entstehen durch positive Gefühle, nicht durch negative!",
             "Jeder kleine Schritt kann der Anfang einer neuen Gewohnheit sein. Und wenn es nicht klappt? Dann machst du ihn noch kleiner!",
             "MIND STEPS unterstützt dich dabei, Verhaltensrezepte zu finden, die für dich funktionieren. \n\n Das wissenschaftlich begründete Prinzip basiert auf dem Verhaltensmodell des promovierten Sozialwissenschaftlers B.J. Fogg von der Stanford University.",
