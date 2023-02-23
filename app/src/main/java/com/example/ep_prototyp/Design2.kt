71ec7c0748e379cf914cf5de0a002ded831108d8
christian.pankiv@smail.th-koeln.de <christian.pankiv@smail.th-koeln.de>
2023-02-24 00:31:53 +0100

    Made ImPOrTanTE ChAnGeSA

--- app/src/main/java/com/example/ep_prototyp/Design2.kt
+++ app/src/main/java/com/example/ep_prototyp/Design2.kt
@@ -6,10 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.ep_prototyp.R
 
 class Design2 : Fragment() {
+    private lateinit var mProfileDatabase:ProfileViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,16 +23,29 @@ class Design2 : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_design2, container, false)
+        mProfileDatabase= ViewModelProvider(this).get(ProfileViewModel::class.java)
+
 
         val button=view.findViewById<Button>(R.id.weiterZuDesign2bButton)
 
         button.setOnClickListener {
-            findNavController().navigate(R.id.action_design2_to_design2b)
+            val eingabe=view.findViewById<EditText>(R.id.editBehaviorSize)
+            promptSpeichern(eingabe)
+            Toast.makeText(requireContext(),"Prompt Gespeichert!", Toast.LENGTH_LONG).show()
 
+            findNavController().navigate(R.id.action_design2_to_design2b)
         }
 
 
-
         return view
+    }
+    private fun promptSpeichern(prompt:EditText) {
+        mProfileDatabase.readRezeptData.observe(viewLifecycleOwner, Observer { data->
+            mProfileDatabase.readData.observe(viewLifecycleOwner,Observer{profil->
+                    mProfileDatabase.updateRezept(Rezept(1,profil[0].goal.toString(),data[0].längeBehaviour,data[0].rezeptCelebration,prompt.text.toString()))
+            })
+        })
+        Toast.makeText(requireContext(),prompt.text,Toast.LENGTH_LONG).show()
+
     }
 }
--- app/src/main/java/com/example/ep_prototyp/Design2b.kt
+++ app/src/main/java/com/example/ep_prototyp/Design2b.kt
@@ -6,28 +6,48 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 
 
 class Design2b : Fragment() {
+    private lateinit var mProfileDatabase:ProfileViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        mProfileDatabase= ViewModelProvider(this).get(ProfileViewModel::class.java)
+
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_design2b, container, false)
 
-        val button = view.findViewById<Button>(R.id.weiterZuDesign3Button)
+        val button=view.findViewById<Button>(R.id.weiterZuDesign3Button)
 
         button.setOnClickListener {
-            findNavController().navigate(R.id.action_design2b_to_design3)
+            val eingabe=view.findViewById<EditText>(R.id.editBehaviorSize)
+            celebrationSpeichern(eingabe)
+            Toast.makeText(requireContext(),"Länge gespeichert!", Toast.LENGTH_LONG).show()
 
+            findNavController().navigate(R.id.action_design2b_to_design3)
         }
 
 
 
+
         return view
+
+    }
+    private fun celebrationSpeichern(celebration: EditText) {
+        mProfileDatabase.readRezeptData.observe(viewLifecycleOwner, Observer { data->
+            mProfileDatabase.readData.observe(viewLifecycleOwner,Observer{profil->
+                    mProfileDatabase.updateRezept(Rezept(1,profil[0].goal.toString(),data[0].längeBehaviour,celebration.text.toString(),data[0].rezeptPrompt))
+            })
+        })
+        Toast.makeText(requireContext(),celebration.text, Toast.LENGTH_LONG).show()
 
     }
 }
