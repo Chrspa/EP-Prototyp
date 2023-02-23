2ae0cb9452e2e54430ff035e40b7515da9c1c9ee
annikalenneper <annika.lenneper@smail.th-koeln.de>
2023-02-23 11:12:52 +0100

    navigation

--- app/src/main/java/com/example/ep_prototyp/review2.kt
+++ app/src/main/java/com/example/ep_prototyp/review2.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.example.ep_prototyp.R
 
 class Review2 : Fragment() {
 
@@ -15,6 +18,8 @@ class Review2 : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
 
-        return inflater.inflate(R.layout.fragment_review2, container, false)
+        val view = inflater.inflate(R.layout.fragment_review2, container, false)
+
+        return view
     }
 }
