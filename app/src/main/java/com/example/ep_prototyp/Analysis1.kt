1db34262bf4dff3dc59978367035096b2ecf8a54
annikalenneper <annika.lenneper@smail.th-koeln.de>
2023-02-23 17:47:11 +0100

    Pop-Up-Fenster angelegt, in Analysis1 initialisiert, Beispiel-Buttons für Pop-up in allen Layouts eingefügt

--- app/src/main/java/com/example/ep_prototyp/Analysis1.kt
+++ app/src/main/java/com/example/ep_prototyp/Analysis1.kt
@@ -5,13 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.widget.Button
+import android.widget.PopupWindow
 import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 
 
 class Analysis1 : Fragment() {
 
+    private lateinit var popupWindow: PopupWindow
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,8 +22,17 @@ class Analysis1 : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_analysis1, container, false)
-
+        val popupView = inflater.inflate(R.layout.pop_up_zusatzinfo_layout, null)
         val button=view.findViewById<Button>(R.id.weiterZuAnalysis2Button)
+
+        popupWindow = PopupWindow(popupView, WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT)
+
+        val popupHeader = popupView.findViewById<TextView>(R.id.textViewExamplesHeader)
+        val popUpExamples = popupView.findViewById<TextView>(R.id.textViewExamples)
+
+        //Pop-Up-TextViews befüllen
+        popupHeader.text = "Hier einige Beispiel-Ziele:"
+        popUpExamples.text = getString(R.string.examplesGoal)
 
         button.setOnClickListener {
             //wenn noch kein Ziel gespeichert, sollte kein "Weiter" möglich sein
