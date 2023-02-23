07260a5e7425b2d064abcf2fb6a66c23da153c7d
annikalenneper <annika.lenneper@smail.th-koeln.de>
2023-02-23 10:34:34 +0100

    zusätzliches Fragment für Celebration angelegt

--- app/src/main/java/com/example/ep_prototyp/Design2b.kt
+++ app/src/main/java/com/example/ep_prototyp/Design2b.kt
@@ -7,26 +7,27 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
-import com.example.ep_prototyp.R
 
-class Design2 : Fragment() {
+
+class Design2b : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_design2, container, false)
+        val view = inflater.inflate(R.layout.fragment_design2b, container, false)
 
-        val button=view.findViewById<Button>(R.id.weiterZuDesign2bButton)
+        val button = view.findViewById<Button>(R.id.weiterZuDesign3Button)
 
         button.setOnClickListener {
-            findNavController().navigate(R.id.action_design2_to_design2b)
+            findNavController().navigate(R.id.action_design2b_to_design3)
 
         }
 
 
 
         return view
+
     }
 }
